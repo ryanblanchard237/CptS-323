cb1aa9e5041223636240cb15f2bbcf820f013377
Ryan C Blanchard <36056811+ryanblanchard237@users.noreply.github.com>
2021-03-31 17:55:47 -0700

    Create Prospect.cs

---
+++ Prospect.cs
@@ -0,0 +1,10 @@
+
+namespace Project_Namespace
+{
+    public class Prospect
+    {
+        public string Name { get; set; }
+        public string Address { get; set; }
+        public bool Accepted { get; set; }
+    }
+}

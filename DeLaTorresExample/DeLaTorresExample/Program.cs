9c5c9bce38d15a48eedf25f66db882a1d8879fe1
Ryan C Blanchard <36056811+ryanblanchard237@users.noreply.github.com>
2021-04-06 20:57:20 -0700

    Simple functional thing using the Firebase.Database package
    This is my own functional example from the code provided by Dr. DeLaTorre
    in the PDF (I think specifically the Milestone 2 PDF) for this project.
    
    I just wanted to try to get something working using the Firebase.Database package,
    rather than the FireSharp package I had been using.

---
+++ DeLaTorresExample/DeLaTorresExample/Program.cs
@@ -0,0 +1,54 @@
+﻿using Firebase.Database;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net.Http;
+
+namespace DeLaTorresExample
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			Run().Wait();
+		}
+
+		private static async Task Run()
+		{
+			// *** Initialization *** //
+			var client = new FirebaseClient("https://cpts323-kovidkillers-default-rtdb.firebaseio.com/");
+			//HttpClient httpclient = new HttpClient();
+
+			// *** Get initial list of people *** //
+			var timeslots = await client.Child("Prospects").OnceAsync<Prospect>();
+			//
+			// This is the problem I was having using the FireSharp package.
+			// I couldn't specify a folder in my database to get, I could only get
+			// I guess what you might call "bottom-level" items.
+			// I.e., I could only get actual items, and not folders-of-items.
+			//
+			// blah.Child("foo")
+			// lets you get a folder.
+
+			foreach (var slot in timeslots)
+			{
+				// The way I have it set up right now, the "Name" of the Prospect
+				// is the key for that prospect in the database.
+				//
+				// And then the corresponding value for that prospect
+				// is the whole set of attributes (fields, variables, whatever) that
+				// the Prospect class has.
+
+				// Console.WriteLine("Name: %d\n", slot.Key);
+				// C# uses a different string format method than printf() in C.
+				Console.WriteLine("Name: {0}", slot.Key);
+				Console.WriteLine("Address: {0}\n", slot.Object.Address);
+			}
+
+			Console.WriteLine("(hit enter to quit)");
+			Console.ReadLine();
+		}
+	}
+}
---
+++ DeLaTorresExample/DeLaTorresExample/Prospect.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DeLaTorresExample
+{
+	class Prospect
+	{
+		public string Name { get; set; }
+		public string Address { get; set; }
+		public bool Accepted { get; set; }
+
+		// public bool VisitInProgress { get; set; }
+		// // Maybe when a team gets to a client's (prospect's) house,
+		// // they confirm their arrival using our software,
+		// // and when they confirm their arrival this bool becomes true
+		// // to indicate that a team is currently with this particular client.
+		// public string AcceptedBy { get; set; }
+		// // Maybe when a team accepts a client (ie. when the client is added to
+		// // the team's list of upcoming clients),
+		// // this string gets set to be the name of the team that accepted the client
+		// // so that the overall system knows what teams are scheduled with what clients.
+		//
+		// Currently leaving these commented out so I can test the stuff I have.
+	}
+}

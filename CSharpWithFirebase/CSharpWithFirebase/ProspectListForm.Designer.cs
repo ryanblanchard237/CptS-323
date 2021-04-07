8dfe031690e6067aae8baa1a1ed95fc5df690a74
Ryan C Blanchard <36056811+ryanblanchard237@users.noreply.github.com>
2021-04-06 17:23:54 -0700

    2nd version of CSharpWithFirebase
    Uses GMap.NET (GMap.NET.WindowsForms and GMap.NET.Presentation can be found using the NuGet package manager) for a maps utility.
    
    Uses FireSharp to talk to Firebase. I didn't realize that dr. delatorre provides examples that uses a diffferent thing, Firebase.Database... might switch to using that.

---
+++ CSharpWithFirebase/CSharpWithFirebase/ProspectListForm.Designer.cs
@@ -0,0 +1,85 @@
+﻿namespace CSharpWithFirebase
+{
+	partial class ProspectListForm
+	{
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+		private System.ComponentModel.IContainer components = null;
+
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		/// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+		protected override void Dispose(bool disposing)
+		{
+			if (disposing && (components != null))
+			{
+				components.Dispose();
+			}
+			base.Dispose(disposing);
+		}
+
+		#region Windows Form Designer generated code
+
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		private void InitializeComponent()
+		{
+			this.listView1 = new System.Windows.Forms.ListView();
+			this.buttonUpdateProspectList = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
+			this.SuspendLayout();
+			// 
+			// listView1
+			// 
+			this.listView1.HideSelection = false;
+			this.listView1.Location = new System.Drawing.Point(13, 13);
+			this.listView1.Name = "listView1";
+			this.listView1.Size = new System.Drawing.Size(425, 536);
+			this.listView1.TabIndex = 0;
+			this.listView1.UseCompatibleStateImageBehavior = false;
+			// 
+			// buttonUpdateProspectList
+			// 
+			this.buttonUpdateProspectList.Location = new System.Drawing.Point(445, 13);
+			this.buttonUpdateProspectList.Name = "buttonUpdateProspectList";
+			this.buttonUpdateProspectList.Size = new System.Drawing.Size(123, 23);
+			this.buttonUpdateProspectList.TabIndex = 1;
+			this.buttonUpdateProspectList.Text = "Update prospect list";
+			this.buttonUpdateProspectList.UseVisualStyleBackColor = true;
+			this.buttonUpdateProspectList.Click += new System.EventHandler(this.buttonUpdateProspectList_Click);
+			// 
+			// label1
+			// 
+			this.label1.AutoSize = true;
+			this.label1.Location = new System.Drawing.Point(444, 39);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(194, 26);
+			this.label1.TabIndex = 2;
+			this.label1.Text = "(Pulls a fresh version of the Prospect list\r\nfrom the remote database.)\r\n";
+			// 
+			// ProspectListForm
+			// 
+			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+			this.ClientSize = new System.Drawing.Size(647, 561);
+			this.Controls.Add(this.label1);
+			this.Controls.Add(this.buttonUpdateProspectList);
+			this.Controls.Add(this.listView1);
+			this.Name = "ProspectListForm";
+			this.Text = "ProspectListForm";
+			this.ResumeLayout(false);
+			this.PerformLayout();
+
+		}
+
+		#endregion
+
+		private System.Windows.Forms.ListView listView1;
+		private System.Windows.Forms.Button buttonUpdateProspectList;
+		private System.Windows.Forms.Label label1;
+	}
+}
---
+++ CSharpWithFirebase/CSharpWithFirebase/ProspectListForm.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using FireSharp.Config;
+using FireSharp.Interfaces;
+using FireSharp.Response;
+
+namespace CSharpWithFirebase
+{
+	public partial class ProspectListForm : Form
+	{
+		public ProspectListForm()
+		{
+			InitializeComponent();
+		}
+
+		IFirebaseConfig fconfig = new FirebaseConfig()
+		{
+			AuthSecret = "Bn3c3rsghHfoHX7PeZwmP5GGrDuywJCiUhQgQFTW",
+			BasePath = "https://cpts323-kovidkillers-default-rtdb.firebaseio.com/"
+		};
+
+		IFirebaseClient client;
+
+		private void ProspectListForm_Load(object sender, EventArgs e)
+		{
+			try
+			{
+				client = new FireSharp.FirebaseClient(fconfig);
+			}
+			catch
+			{
+				MessageBox.Show("There was a problem creating a FirebaseClient from the given FirebaseConfig.");
+			}
+		}
+
+		private void buttonUpdateProspectList_Click(object sender, EventArgs e)
+		{
+			// FirebaseResponse r = client.Get("Prospects");
+			//                          //.GetAsync() ?
+			// 						 //     "cpts323-kovidkillers-default-rtdb"
+			// // Using either "cpts323-kovidkillers-default-rtdb" or "Prospects",
+			// // it doesn't like it.
+			// //
+			// // I keep getting a NullReferenceException...
+			// // "Object reference not set to an instance of an object."
+
+
+			// FirebaseResponse response = await client.GetAsync("something");
+			// 
+			// https://stackoverflow.com/q/51579134 , "How to return as objects from firebase using firesharp"
+
+
+			// var response = client.Get("Prospects/");
+			// // This should get me everything in the "Prospects" directory in the database.
+			// 
+			// // Try this for now to see what happens.
+			// MessageBox.Show(r.Body);
+
+
+
+			// Step 1. Figure out how we get all the prospects. Abd what kind of a data structure this is.
+
+			var result = client.Get("Prospects");
+
+			MessageBox.Show("Result of \"client.Get(\"Prospects\")\" as just a string...\n"
+				            + 
+				            result.Body);
+		}
+	}
+}

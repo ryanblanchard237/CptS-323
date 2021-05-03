b4c086d6ff239f8e8588d4852e13e6be2e2d8423
Ryan C Blanchard <36056811+ryanblanchard237@users.noreply.github.com>
2021-05-02 17:43:01 -0700

    Add files via upload

---
+++ 2021-04-30-ryan/Home_Visits_Vaccination/Home_Visits_Vaccination/Classes/Company.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Visits_Vaccination
+{
+    public class Company
+    {
+        public string companyName;
+        public string status;
+    }
+}
---
+++ 2021-04-30-ryan/Home_Visits_Vaccination/Home_Visits_Vaccination/Classes/Destination.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Home_Visits_Vaccination
+{
+    public class Destination
+    {
+        public double lat;
+        public double lon;
+        public string destinationName;
+        //public ServerTimeStamp TimestampPlaceholder { get; } = new ServerTimeStamp();
+    }
+}

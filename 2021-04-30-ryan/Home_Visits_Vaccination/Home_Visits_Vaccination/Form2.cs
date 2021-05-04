ae61974c60990784c337628f9381691f9c09d128
Jdtot <46335478+Jdtot@users.noreply.github.com>
2021-05-03 22:14:33 -0700

    Add files via upload
    Animate replaced example2, if we use the get patients first it will use the first patient from the list.
    -Still need to get the fire base codes for when we select a patient and start/finish
    -Either the map is slightly off or the math functions he gives is slightly miscalculating the car, it turns correctly but is always off the route slightly
    -As far as I can tell once we use the get patient once it will keep updating the list, so we can change it to call it once at the start and it should keep updating

--- 2021-04-30-ryan/Home_Visits_Vaccination/Home_Visits_Vaccination/Form2.cs
+++ 2021-04-30-ryan/Home_Visits_Vaccination/Home_Visits_Vaccination/Form2.cs
@@ -12,11 +12,9 @@ using System.Drawing.Drawing2D;
 using System.Globalization;
 using System.IO;
 using System.Net;
-
 using FireSharp.Config;
 using FireSharp.Interfaces;
 using FireSharp.Response;
-
 using GMap;
 using GMap.NET;
 using GMap.NET.MapProviders;
@@ -83,35 +81,6 @@ namespace Home_Visits_Vaccination
 			// Also have to keep in mind we might be setting markers on it.
 			// Although now that I think about it,
 		}
-
-		private double Distance(PointLatLng p1, PointLatLng p2)
-		{
-			//radius of earth in meters
-			double R = 6371e3;
-
-			double l1 = p1.Lat * Math.PI / 180;
-			double l2 = p2.Lat * Math.PI / 180;
-
-			double theta = (p2.Lat - p1.Lat) * Math.PI / 180;
-
-			double lambda = (p2.Lng - p1.Lng) * Math.PI / 180;
-
-			double a = Math.Sin(theta / 2) * Math.Sin(theta / 2)
-				+ Math.Cos(l1) * Math.Cos(l2) *
-				Math.Sin(lambda / 2) * Math.Sin(lambda / 2);
-
-			double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
-
-
-			double m = R * c; // get distance in meters
-
-
-			return m / 1609.344; //returns distance in miles
-
-		}
-
-
-
 
 		public static async Task FromMilestone4PDF()
 		{
@@ -215,75 +184,176 @@ namespace Home_Visits_Vaccination
 				Thread.Sleep(1000);
 				Console.WriteLine("Point {0} coords are: {1}", i, mapRoute2.Points[i]);
 			}
-			/*// This will work to get directions from one point to another routeDirection will contain the steps which we can use to display position.
-			// Also will include the time for the trip. Can use this to compare and find the optimal prospect.
-
-			GMapProviders.GoogleMap.ApiKey = "AIzaSyD_EgYiqKMGksVC9LqJeRqx5WqRByy6nLk"; //"AIzaSyBsS4_zQy-svXOtLrS32XPphEsSX-EMY8M"; // that didnt help.
-			PointLatLng start = new PointLatLng(46.289106, -119.292999);
-			PointLatLng end = new PointLatLng(46.276860, -119.291511);
-
-			GMapProviders.GoogleMap.GetDirections(out routeDirection, start, end, false, false, false, false, false);
-			//routeDirection.Route
-
-			// Just make a bunch of points to see if the other thing even works.
-			// (ie if its just the thing thats not the other thing that has a problem.)
-			//
-			List<PointLatLng> pointLatLngs = new List<PointLatLng>();
-			pointLatLngs.Add(new PointLatLng(46.209161, -119.176587)); // Kennewick
-			pointLatLngs.Add(new PointLatLng(46.203846, -119.186387)); // Kennewick
-			pointLatLngs.Add(new PointLatLng(46.209517, -119.203108)); // Kennewick
-			pointLatLngs.Add(new PointLatLng(46.232833, -119.104462)); // Pasco
-			pointLatLngs.Add(new PointLatLng(46.245739, -119.130955)); // Pasco
-			pointLatLngs.Add(new PointLatLng(46.284001, -119.295114)); // Richland
-
-			GMapRoute gMapRoute1 = new GMapRoute(pointLatLngs, "gMapRoute3");
-			GMapOverlay overlay1 = new GMapOverlay("Test Route");
-			overlay1.Routes.Add(gMapRoute1);
-			gMapControl1.Overlays.Add(overlay1); //x
-			gMapControl1.Refresh();
-
-			GMarkerGoogle startP = new GMarkerGoogle(start, GMarkerGoogleType.red_pushpin);// Adds the markers to start and end points.
-			GMarkerGoogle endP = new GMarkerGoogle(end, GMarkerGoogleType.red_pushpin);
-
-
-
-			GMarkerGoogle car = new GMarkerGoogle(pointLatLngs[1], new Bitmap(@"C:\Users\Jason\Desktop\323_2\CptS-323-main\CptS-323-main\2021-04-30-ryan\Home_Visits_Vaccination\car-2.png")); // better.
-			// 
-			// Maybe don't need to down-scale the size of the car now.
-			// (I shrunk the original image and called that result file "car-2".
-			// 
-			// System.Drawing.Size carOldSize = car.Size;
-			// textBox1.Text = $"Current values of car.Size...\r\ncar.Size.Width = {carOldSize.Width}\r\ncar.Size.Height = {carOldSize.Height}\r\n";
-			// 
-			// System.Drawing.Size carNewSize = new System.Drawing.Size();
-			// double carScaleFactor = 0.1;
-			// carNewSize.Width = (int)(carScaleFactor * carOldSize.Width);
-			// carNewSize.Height = (int)(carScaleFactor * carOldSize.Height);
-			// string textBox1OldText = textBox1.Text;
-			// textBox1.Text = textBox1OldText + $"car new size...\r\nWidth = {carNewSize.Width}\r\nHeight = {carNewSize.Height}\r\n";
-			// 
-			// car.Size = carNewSize;
-
-
-
-			GMapOverlay overlay2 = new GMapOverlay("car");
-			overlay2.Markers.Add(car);
-			gMapControl1.Overlays.Add(overlay2);
-
-			int i=0;
-			while (true)
-			{
-				car.Position = pointLatLngs[i];
-				gMapControl1.Refresh();
-				Thread.Sleep(1000);
-				
-				i++;
-				if (i == pointLatLngs.Count)
-					break;
-			}
-			*/
+	
 		}
 
+
+		public double Bearing(PointLatLng p1, PointLatLng p2)
+		{
+			//Convert input values to radians   
+			var lat1 = DegreeToRadian(p1.Lat);
+			var long1 = DegreeToRadian(p1.Lng);
+			var lat2 = DegreeToRadian(p2.Lat);
+			var long2 = DegreeToRadian(p2.Lng);
+			double deltaLong = long2 - long1;
+			double y = Math.Cos(lat2) * Math.Sin(deltaLong);
+			double x = Math.Cos(lat1) * Math.Sin(lat2) - Math.Sin(lat1) * Math.Cos(lat2) * Math.Cos(deltaLong); ;
+			double bearing = Math.Atan2(y, x);
+			return bearing;
+		}
+		public class GMapMarkerVan : GMarkerGoogle
+		{
+
+			private readonly Bitmap icon = new Bitmap(Path.Combine(Application.StartupPath, @"C:\\Users\\Jason\\Desktop\\323\\Milestone2\\CptS-323-main (1)\\CptS-323-main\\Home_Visits_Vaccination\\Home_Visits_Vaccination\\Car_Icon4.png"));
+			//private readonly Bitmap icon = new Bitmap(Path.Combine(Application.StartupPath, Path.Combine(Application.StartupPath, @"..\..\..\..\Car_Icon4.png")));
+			float heading = 0;
+			public GMapMarkerVan(PointLatLng p, Bitmap car) :base(p,car)
+			{
+				this.heading = 0;
+				Size = icon.Size;
+			}
+			public void Rotate(float degre)
+			{
+				this.heading = degre;
+			}
+			public override void OnRender(Graphics g)
+			{
+				Matrix temp = g.Transform;
+				g.TranslateTransform(LocalPosition.X, LocalPosition.Y);
+				g.RotateTransform(-Overlay.Control.Bearing);
+
+				try
+				{
+					g.RotateTransform(heading);
+				}
+				catch { }
+
+				g.DrawImageUnscaled(icon, -icon.Width / 4, -icon.Height / 4);
+				g.Transform = temp;
+			}
+		}
+		public double DegreeToRadian(double angle)
+		{
+			return Math.PI * angle / 180.0;
+		}
+
+
+	
+	public void animate(int index)
+
+		{
+			GMapProviders.GoogleMap.ApiKey = "AIzaSyBsS4_zQy-svXOtLrS32XPphEsSX-EMY8M";
+			PointLatLng start, end;
+			start = new PointLatLng(46.289106, -119.292999);
+			if (myappointments.Count != 0)
+			{
+				for (int j = 0; j < 4; j++)
+				{
+					{
+						//start = new PointLatLng(myappointments[0].origin.lat, myappointments[0].origin.lon);
+						myappointments[j].destination.lon = -119.111432;
+						end = new PointLatLng(myappointments[0].destination.lat, myappointments[0].destination.lon);
+					}
+			//else
+					//{
+						//start = new PointLatLng(46.289106, -119.292999); // Will pass the selected appointment lat/lng here
+					//	end = new PointLatLng(46.276860, -119.290511);
+					//}
+					var temp = GMapProviders.GoogleMap.GetDirections(out routeDirection, start, end, false, false, false, false, false); // API call to get the directions
+					GMapRoute mapRoute2 = new GMapRoute(routeDirection.Route, "This Trip"); // Creates the route 
+					GMapOverlay overlayTest = new GMapOverlay("Test Route");
+					overlayTest.Routes.Add(mapRoute2);
+					gMapControl1.Overlays.Add(overlayTest);
+					GMap.NET.WindowsForms.Markers.GMarkerGoogle startP = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(start, GMarkerGoogleType.green_dot);// Adds the markers to start and end points.
+					GMap.NET.WindowsForms.Markers.GMarkerGoogle endP = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(end, GMarkerGoogleType.red_dot);
+					Bitmap icon = new Bitmap("C:\\Users\\Jason\\Desktop\\323\\Milestone2\\CptS-323-main (1)\\CptS-323-main\\Home_Visits_Vaccination\\Home_Visits_Vaccination\\Car_Icon4.png");
+					//var carMark = new GMap.NET.WindowsForms.Markers.GMarkerGoogle(mapRoute2.Points[11], new Bitmap("C:\\Users\\Jason\\Desktop\\323\\Milestone2\\CptS-323-main (1)\\CptS-323-main\\Home_Visits_Vaccination\\Home_Visits_Vaccination\\Car_Icon2.png")); // Sets the car marker variable and assigns it the bitmap (Icon)
+					var carMark = new GMapMarkerVan(mapRoute2.Points[mapRoute2.Points.Count - 1], icon);
+					carMark.Position = mapRoute2.Points[0];
+					objects.Markers.Add(startP); // Adds to markers list
+					objects.Markers.Add(endP);  // adds to markers list
+					objects.Markers.Add(carMark); // Add the car marker to overlay.
+
+					gMapControl1.Overlays.Add(objects); // Adds the markers to actual overlay.
+					gMapControl1.ZoomAndCenterRoute(mapRoute2); // zooms to the route.
+					gMapControl1.Refresh();
+					Thread t = new Thread(() =>
+
+					{
+
+
+						var timedelay = 0.016;
+						for (int i = 1; i < mapRoute2.Points.Count; i++)
+						{
+							var degree = Bearing(mapRoute2.Points[i - 1], mapRoute2.Points[i]);
+							var deltaX = Distance(mapRoute2.Points[i - 1], mapRoute2.Points[i]);
+							degree = (degree * 180 / Math.PI + 360) % 360;
+							carMark.Rotate((float)degree);
+							var distance = 0.00972222222; //35mph to miles per second
+						var deltaSeconds = deltaX / distance;
+							deltaSeconds = deltaSeconds / 16;
+						//Console.WriteLine(routeDirection);
+						for (double s = 0; s < deltaSeconds; s = s + timedelay)
+							{
+								var r = s / deltaSeconds;
+								var dlat = r * (mapRoute2.Points[i].Lat) + (1 - r) * (mapRoute2.Points[i - 1].Lat);
+								var dlng = r * (mapRoute2.Points[i].Lng) + (1 - r) * (mapRoute2.Points[i - 1].Lng);
+								var M_point = new PointLatLng(dlat, dlng);
+								Thread.Sleep((int)(deltaSeconds * timedelay * 1000));
+								carMark.Position = M_point;
+							//Console.WriteLine("Point {0} coords are: {1}", i, M_point);
+						}
+							Console.WriteLine("Point {0} coords are: {1}", i, carMark.Position);
+						}
+						Console.WriteLine("Start Sleep 56s");
+						Console.WriteLine("{0}", Path.Combine(Application.StartupPath, @"..\..\..\..\Car_Icon4.png"));
+						Thread.Sleep((int)(56 * 10));
+						Console.WriteLine("DONE. end is {0}", end);
+
+					//firebase to upfate the status 1 (key),
+
+					//Thread.Sleep((int)(56 * 1000));
+
+					//firebase to upfate the status 2 ,
+
+					//call firebase finishAppointment
+
+					//}
+					//=>
+				}
+					);
+					t.Start();
+				}
+			}
+		}
+		private double Distance(PointLatLng p1, PointLatLng p2)
+
+		{
+			//radius of earth in meters
+
+			double R = 6371e3;
+
+			double l1 = p1.Lat * Math.PI / 180;
+
+			double l2 = p2.Lat * Math.PI / 180;
+
+			double theta = (p2.Lat - p1.Lat) * Math.PI / 180;
+
+			double lambda = (p2.Lng - p1.Lng) * Math.PI / 180;
+
+			double a = Math.Sin(theta / 2) * Math.Sin(theta / 2)
+
+				+ Math.Cos(l1) * Math.Cos(l2) *
+
+				Math.Sin(lambda / 2) * Math.Sin(lambda / 2);
+
+			double c = 2 * Math.Atan2(Math.Sqrt(a), Math.Sqrt(1 - a));
+
+			double m = R * c; // get distance in meters
+
+			return m / 1609.344; //returns distance in miles
+
+		}
 
 
 		public static async Task example1()
@@ -321,7 +391,8 @@ namespace Home_Visits_Vaccination
 			//example1();
 			PointLatLng start = new PointLatLng(46.289106, -119.292999); // Will pass the selected appointment lat/lng here
 			PointLatLng end = new PointLatLng(46.276860, -119.291511);
-			example2(start,end);
+			animate(1);
+			//example2(start,end);
 		}
 
 
@@ -350,26 +421,6 @@ namespace Home_Visits_Vaccination
 			//TalkToFirebase.FromMilestone4PDF();
 		}
 
-		// Add Appointment (1)
-		//
-		// Looks like this class (from Delatorre's code in the Milestone 2 PDF, which Jason copied into some C# files (thanks Jason))
-		// uses a bunch of other custom classes in it,
-		// so Im gonna skip it for now.
-		//
-		//private void buttonAddAppointment_Click(object sender, EventArgs e)
-		//{
-		//	Appointment a = new Appointment()
-		//	{
-		//
-		//	}
-		//}
-
-
-		// Add Patient (4)
-		//
-		// This one looks pretty do-able with a basic Windows Form.
-		// (Just set up a Windows Form that lets you fill in every field and bam.)
-		//
 		private void buttonAddPatient_Click(object sender, EventArgs e)
 		{
 			example1();
@@ -382,152 +433,5 @@ namespace Home_Visits_Vaccination
         {
 
         }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-        //public System.Threading.Tasks.Task DrDelatorresWaytodoit()
-        //{
-        //	//******************** Initialization ***************************//
-        //	var client = new FirebaseClient("https://cpts323battle.firebaseio.com/");
-        //	HttpClient httpclient = new HttpClient();
-        //	string selectedkey = "", responseString, companyId;
-        //	FormUrlEncodedContent content;
-        //	HttpResponseMessage response;
-
-        //	//******************** Get initial list of Prospect ***********************//
-        //	var Appointments = await client.Child("appointments").OnceAsync<Appointment>();
-
-        //	foreach (var appointment in Appointments)
-        //	{
-        //		Console.WriteLine($"OA1:{appointment.Key}:->{appointment.Object.acepted}");
-        //		//create a ilist os appointments
-        //		//smart selection to improve your profit
-        //		selectedkey = appointment.Key;
-        //	}
-        //	//******************** Get Prospect list one by one real time ***************************/
-        //	var child = client.Child("appointments");
-        //	var observable = child.AsObservable<Appointment>();
-        //	//get a new appoinment in the list
-        //	var subscriptionFree = observable
-        //	.Where(f => !string.IsNullOrEmpty(f.Key)) // you get empty Key when there are no data on the server for specified node
-        //	.Where(f => f.Object?.acepted == false)
-        //	.Subscribe(appointment =>
-        //	{
-        //		selectedkey = appointment.Key;
-        //		Console.WriteLine($"New Appoinment:{appointment.Key}:->{appointment.Object.destination.destinationName}");// update the lits of appointsments.
-        //	});
-
-        //	//******************* Cloud Function subcribe a Company ****************/
-        //	var company = new Company
-        //	{
-        //		companyName = "WSU VANS",
-        //		status = "active"
-        //	};
-        //				var dictionary = new Dictionary<string, string>
-        //	{
-        //	{ "companyName",company.companyName },
-        //	{ "status",company.status}
-        //	};
-        //	content = new FormUrlEncodedContent(dictionary);
-        //	response = await httpclient.PostAsync("https://us-central1-cpts323battle.cloudfunctions.net/reportCompany", content);
-        //	responseString = await response.Content.ReadAsStringAsync();
-        //	Response data = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(responseString);
-        //	//Response message
-        //	Console.WriteLine(data.message);
-        //	Console.WriteLine(data.companyId);
-        //	companyId = data.companyId;
-
-        //	//******************* Call Cloud Function select a Appointment By Id ****************/
-        //	dictionary = new Dictionary<string, string>
-        //	{
-        //		{ "key",selectedkey },
-        //		{ "carPlate","AXL234" },
-        //		{ "did","2"},
-        //		{ "company","Company_name"},
-        //		{ "companyId",companyId},
-        //		{ "carStars","4.8"},
-        //		{ "image","http:.."}
-        //	};
-
-        //	content = new FormUrlEncodedContent(dictionary);
-        //	response = await httpclient.PostAsync("https://us-central1-cpts323battle.cloudfunctions.net/selectAppointmentById", content);
-        //	responseString = await response.Content.ReadAsStringAsync();
-        //	data = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(responseString);
-        //	Console.WriteLine(data.message);
-
-        //	//*******************Direct firebase modification example, this is uses only to report the appointment status ****************/
-        //	var child2 = client.Child("/appointmentsStatus/" + selectedkey + "/status/0");
-        //	var status = new Status
-        //	{
-        //		code = 100
-        //	};
-        //	await child2.PutAsync(status);
-        //	var child3 = client.Child("/appointmentsStatus/" + selectedkey + "/status/1");
-        //	status = new Status
-        //	{
-        //		code = 110
-        //	};
-        //	await child3.PutAsync(status);
-
-        //	var child4 = client.Child("/appointmentsStatus/" + selectedkey + "/status/2");
-        //	status = new Status
-        //	{
-        //		code = 120
-        //	};
-
-        //	await child4.PutAsync(status);
-        //	//******************* Call Cloud Function updatePosition ****************/
-        //	dictionary = new Dictionary<string, string>
-        //	{
-        //		{ "key",selectedkey },
-        //		{ "did","12"},
-        //		{ "companyId",companyId},
-        //		{ "lat","46.271135"},
-        //		{ "lng","-119.278556"}
-        //	};
-
-        //	content = new FormUrlEncodedContent(dictionary);
-        //	response = await httpclient.PostAsync("https://us-central1-cpts323battle.cloudfunctions.net/updatePosition", content);
-        //	responseString = await response.Content.ReadAsStringAsync();
-        //	data = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(responseString);
-        //	Console.WriteLine(data.message);
-        //	Console.WriteLine("Current index for the point " + data.index);
-        //	//******************* Call Cloud Function for finishAppointment ****************/
-        //	dictionary = new Dictionary<string, string>
-        //	{
-        //		{ "key",selectedkey },
-        //		{ "did","12"},
-        //		{ "companyId",companyId},
-        //		{ "lat","10.1991"},
-        //		{ "lng","-75.8890"}
-        //	};
-
-        //	content = new FormUrlEncodedContent(dictionary);
-        //	response = await httpclient.PostAsync("https://us-central1-cpts323battle.cloudfunctions.net/finishAppointment", content);
-        //	responseString = await response.Content.ReadAsStringAsync();
-        //	data = Newtonsoft.Json.JsonConvert.DeserializeObject<Response>(responseString);
-        //	Console.WriteLine(data.message);
-        //	var stop = Console.ReadLine();
-        //	subscriptionFree.Dispose();
-        //	//subscriptionSelected.Dispose();
-        //}
-
-
-
-
-
     }
 }
